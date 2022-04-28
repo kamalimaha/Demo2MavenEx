8e6f0eaf6eddf22f1c67264ec950afd7e7a02c99
kamalimaha <kamalimaha5@gmail.com>
2022-04-28 15:31:35 +0530

    first commit

---
+++ src/main/java/com/lti/DataType.java
@@ -0,0 +1,29 @@
+package com.lti;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte rollNo=101;
+		System.out.println("roll no is "+rollNo);
+		int empId=1001;
+		System.out.println("Empid is "+empId);
+		float price=100;
+		System.out.println("price "+price);
+		float totalPrice=100.0f;
+		System.out.println(totalPrice);
+		char ch='A';
+		System.out.println(ch);
+		boolean s=true;
+		System.out.println(s);
+		int a=10;
+		int b=50;
+		System.out.println(a>b);
+		boolean c= a!=b;
+		System.out.println("c "+c);
+		String name="Kamali";
+		System.out.println(name);
+				
+	}
+
+}
